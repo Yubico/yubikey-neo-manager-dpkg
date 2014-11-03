b202a31405bf9352d2e33972f013e6ed423430cb
Dain Nilsson <dain@yubico.com>
2014-11-03 12:06:20 +0100

    Imported Upstream version 0.2.5

--- neoman/qt_resources.py
+++ neoman/qt_resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon Oct 27 14:41:11 2014
+# Created: Mon Nov 3 10:59:35 2014
 #      by: The Resource Compiler for PySide (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
